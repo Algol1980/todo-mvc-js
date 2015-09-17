313e4291a0b9c3ac6e71d6ed71ae87095b220d66
Rus <rusmailkgnight@gmail.com>
2015-09-17 21:23:22 +0300

    Add element

--- js/app.js
+++ js/app.js
@@ -17,3 +17,41 @@
 // var $body = qs('body');
 // $delegate( $body, 'input[type=text]', 'focus', consoleValue);
 // $delegate( $body, 'input[type=text]', 'blur', consoleValue);
+
+
+var todos = [];
+var $newTodoText = qs('#newTodoText');
+var $todosList = qs('#todosList');
+//функция keyupHandler
+var keyupHandler = function keyupHandler(event) {
+	
+	//проверки что это ENTER
+	if ( event.keyCode === 13 ) {
+
+		//сохранение элемента в массиве
+		todos.push({
+			complited: false,
+			text: event.target.value
+		});
+
+		//создание li
+		var li = document.createElement('li');
+		
+		//создание checkbox
+		var checkbox = document.createElement('input');
+		checkbox.setAttribute('type', 'checkbox');
+
+		//создание label
+		var label = document.createElement('label');
+		label.innerHTML = event.target.value;
+
+
+		li.appendChild( checkbox );
+		li.appendChild( label );
+
+		//добавить li в ul
+		$todosList.appendChild(li);
+	}
+};
+//событие keyup
+$on($newTodoText, 'keyup', keyupHandler);
