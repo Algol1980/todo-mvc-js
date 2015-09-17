637ce2ba1f295c864125fa78df9947bed39628ec
Rus <rusmailkgnight@gmail.com>
2015-09-17 20:35:02 +0300

    Clear

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
-var consoleClick = function(event) {
-	console.log(event);
-};
-$on( qs('#clear-complited'), 'click', consoleClick);
+// var consoleClick = function(event) {
+// 	console.log(event);
+// };
+// $on( qs('#clear-complited'), 'click', consoleClick);
 
-var b = document.createElement('button');
-$on( b, 'click', consoleClick);
-b.innerHtml = 'new';
+// var b = document.createElement('button');
+// $on( b, 'click', consoleClick);
+// b.innerHtml = 'new';
 
-qs('#listTodo').appendChild(b);
+// qs('#listTodo').appendChild(b);
 
-$delegate( qs('#listTodo'), '.cl', 'click', consoleClick);
+// $delegate( qs('#listTodo'), '.cl', 'click', consoleClick);
+
+// function consoleValue(event) {
+// 	console.log( event.target.value);
+// }
+// var $body = qs('body');
+// $delegate( $body, 'input[type=text]', 'focus', consoleValue);
+// $delegate( $body, 'input[type=text]', 'blur', consoleValue);
--- js/helper.js
+++ js/helper.js
@@ -22,7 +22,8 @@
 		function dispatchEvent(event) {
 			var targetElement = event.target;
 			var potentialElements = window.qsa(selector, target);
-			var hasMatch = Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
+			var hasMatch = Array.prototype.indexOf.call(
+				potentialElements, targetElement) >= 0;
 
 			if (hasMatch) {
 				handler.call(targetElement, event);
